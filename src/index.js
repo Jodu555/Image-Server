07cb3defbfa7f89d8c8f8cce6cb322e4de07d534
Jodu555 <Jodu505@gmail.com>
2021-08-16 20:55:52 +0200

    Fixed some helmet invoke bugs + tried with upload with multer

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const multer = require('multer');
 
 const app = express();
 app.use(cors());
 app.use(morgan('dev'));
-app.use(helmet);
+app.use(helmet());
 app.use(express.json());
 
+const upload = multer({ dest: 'uploads/' })
+app.post('/file', upload.single('d'), (req, res) => {
+    console.log(req.file);
+    res.send({
+        success: true,
+    });
+});
 
 
 const PORT = process.env.PORT || 3100;
